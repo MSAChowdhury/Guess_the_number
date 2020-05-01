554f4f16b1bb43cc0a9c00cedfc688b76ddc6170
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-05-02 04:38:11 +0600

    Ready to play

--- Game_Project1.py
+++ Game_Project1.py
@@ -1,29 +1,19 @@
-###########################
-## PART 10: Simple Game ###
-### --- CODEBREAKER --- ###
-## --Nope--Close--Match--  ##
-###########################
 
-# It's time to actually make a simple command line game so put together everything
-# you've learned so far about Python. The game goes like this:
 
-# 1. The computer will think of 3 digit number that has no repeating digits.
-# 2. You will then guess a 3 digit number
-# 3. The computer will then give back clues, the possible clues are:
-#
-#     Close: You've guessed a correct number but in the wrong position
-#     Match: You've guessed a correct number in the correct position
-#     Nope: You haven't guess any of the numbers correctly
-#
-# 4. Based on these clues you will guess again until you break the code with a
-#    perfect match!
+
+
+
 import random
-print("Welcome! Code Breakers. Here, you have to break the 3-digits code with your ready wit. Let's see can you break it ASAP")
+print("***********************\n*** PART 10: Simple Game ***\n*** --- CODEBREAKER --- ***\n** --Nope--Close--Match--  **\n**************************\n")
+print("\nWelcome! Code Breakers. Here, you have to break the 3-digits code with your ready wit. Let's see can you break it ASAP\n\n")
+print("1. The computer will think of 3 digit number that has no repeating digits.\n2. You will then guess a 3 digit number\n3. The computer will then give back clues, the possible clues are:\n\nClose: You've guessed a correct number but in the wrong position\n\nMatch: You've guessed a correct number in the correct position\n\nNope: You haven't guess any of the numbers correctly\n\n4. Based on these clues you will guess again until you break the code with a perfect match!")
+
+
 
 def gen_code():
     digits = list(range(10))
     random.shuffle(digits)
-    print(digits[3:6])
+    #print(digits[3:6])
     return (digits[3:6])
 
 def number_check(com_code):
